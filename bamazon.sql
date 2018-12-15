cec7ee3270e70683d4e3f213f6eec5dc5e366c30
Thomas J Calhoun <thomascalhoun@Thomass-MacBook-Pro.local>
2018-12-15 00:20:31 -0800

    added logic to js files and created tables for node app with requireing console.table

--- bamazon.sql
+++ bamazon.sql
@@ -1,4 +1,4 @@
-DATA DATABASE IF EXISTS bamazonDB;
+DROP DATABASE IF EXISTS bamazonDB;
 
 CREATE database bamazonDB;
 
@@ -10,20 +10,23 @@ CREATE TABLE products (
     department_name VARCHAR(45) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     stock_quantity INT NOT NULL,
+    PRIMARY KEY (item_id)
 );
 
 SELECT * FROM products;
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES 
-("christmas tree", "holidays", 199.99, 52);
-("mountain bike", "sports", 599.99, 19);
-("tooth brush", 'self care' 2.99, 85);
-("champagne", "food and drink", 12.99, 13);
-("la croix", "food and drink", 6.99, 29);
-("rubber duck", "toys", 1.99, 150);
-("alarm clock", "bedroom", 12.99, 65);
-("frozen pizza", "food and drink", 8.99, 14);
-("football", "sports", 9.99, 103);
+("christmas tree", "holidays", 199.99, 52),
+("mountain bike", "sports", 599.99, 19),
+("tooth brush", 'self care', 2.99, 85),
+("champagne", "food and drink", 12.99, 13),
+("la croix", "food and drink", 6.99, 29),
+("rubber duck", "toys", 1.99, 150),
+("alarm clock", "bedroom", 12.99, 65),
+("frozen pizza", "food and drink", 8.99, 14),
+("football", "sports", 9.99, 103),
 ("christmas lights", "holidays", 15.99, 30);
-INSERT INTO products VALUES
+
+
+INSERT INTO products VALUES;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,29 +1,70 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+const cTable = require('console.table');
 
 var connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3306
   port: 3306,
 
-  // Your username
   user: "root",
 
-  // Your password
   password: "",
   database: "bamazonDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  afterConnection();
+  afterConnected();
 });
 
-function afterConnection() {
-  connection.query("SELECT * FROM products", function(err, res) {
+function afterConnected() {
+  connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
-    console.log(res);
+
+    else {
+
+      console.table("\nProducts Availible", res)
+
+      inquirer.prompt([{
+        name: "ID",
+        message: "Please select the item id of the product you would like to purchase?"
+      }, {
+        name: "units",
+        message: "How many do you want?"
+      }]).then(function (answer) {
+
+        var userAnswer = answer.ID;
+        var checkID = res[(userAnswer) - 1].item_id;
+        var productName = res[(userAnswer) - 1].product_name;
+
+        console.log("\nInvoice")
+        console.log("---------------------------");
+        console.log("You Requested: " + productName);
+
+        if (userAnswer = checkID) {
+          console.log("In-stock quantity: " + res[(userAnswer) - 1].stock_quantity);
+          console.log("You requested : " + answer.units + " units");
+          var checkingStock = res[(userAnswer) - 1].stock_quantity;
+          var answerUnits = answer.units;
+          if (checkingStock < answerUnits) {
+            console.log('Insufficient quantity, please try again');
+
+          } else {
+            checkingStock = checkingStock - answerUnits;
+            console.log("Left in stock: " + checkingStock);
+            var productPrice = res[(userAnswer) - 1].price;
+
+
+            console.log("Price per item: " + productPrice);
+            var totalCost = productPrice * answerUnits;
+            console.log("---------------------------")
+            console.log("Total Cost Today: " + totalCost);
+            console.log("---------------------------");
+          }
+        }
+      })
+    };
     connection.end();
   });
-}
+};
